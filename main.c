9d73718e348f099637700714a16791253a511ec1
Armin Biere <biere@jku.at>
2017-10-11 07:42:50 +0200

    bigger example

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include "headers.h"
 
 int main (int argc, char ** argv) {
-  Circuit * c = new_circuit (4);
-  Gate * a = new_and_gate (c);
-  Gate * x = get_input_gate (c, 1);
-  Gate * y = get_input_gate (c, 2);
-  connect_gates (x, a);
-  connect_gates (y, a);
-  connect_output (c, a);
+  const int num_numinputs = 4;
+  Circuit * c = new_circuit (num_numinputs);
+  Gate * input[num_numinputs + 1];
+  for (int i = 1; i <= num_numinputs; i++)
+    input[i] = get_input_gate (c, i);
+  Gate * f = new_and_gate (c);
+  connect_gates (input[1], f);
+  connect_gates (input[2], f);
+  Gate * g = new_xor_gate (c);
+  connect_gates (input[3], g);
+  connect_gates (input[4], g);
+  Gate * o = new_and_gate (c);
+  connect_gates (NOT (f), o);
+  connect_gates (NOT (g), o);
+  connect_output (c, NOT (o));
   cone_of_influence (c);
   delete_circuit (c);
   print_statistics ();
