5a8a9236829b3c5be882760044f811f4b1af52d1
Armin Biere <biere@jku.at>
2017-10-23 08:35:35 +0200

    imported catch signal

--- signal.c
+++ signal.c
@@ -1,7 +1,55 @@
 #include "headers.h"
 
-void set_signal_handlers () {
-}
+#include <signal.h>
+
+static volatile int catched_signal;
+
+#define SIGNALS \
+SIGNAL(SIGINT) \
+SIGNAL(SIGSEGV) \
+SIGNAL(SIGABRT) \
+SIGNAL(SIGTERM) \
+SIGNAL(SIGBUS) \
+
+#define SIGNAL(SIG) \
+static void (*SIG ## _handler)(int);
+SIGNALS
+#undef SIGNAL
 
 void reset_signal_handlers () {
+#define SIGNAL(SIG) \
+  (void) signal (SIG, SIG ## _handler);
+  SIGNALS
+#undef SIGNAL
+  catched_signal = 0;
 }
+
+static const char * signal_name (int sig) {
+#define SIGNAL(SIG) \
+  if (sig == SIG) return # SIG; else
+  SIGNALS
+#undef SIGNAL
+  return "UNKNOWN";
+}
+
+static void catch_signal (int sig) {
+  if (!catched_signal) {
+    catched_signal = 1;
+    msg (1, "");
+    msg (1, "CAUGHT SIGNAL %d %s", sig, signal_name (sig));
+    msg (1, "result");
+    msg (1, "s UNKNOWN");
+    print_statistics ();
+  }
+  msg (1, "RERAISING SIGNAL %d %s", sig, signal_name (sig));
+  reset_signal_handlers ();
+  raise (sig);
+}
+
+void set_signal_handlers () {
+#define SIGNAL(SIG) \
+  SIG ## _handler = signal (SIG, catch_signal);
+  SIGNALS
+#undef SIGNAL
+}
+
