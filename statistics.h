e99a6c61d1942775e927d34edbcf670d677b576d
Armin Biere <biere@jku.at>
2018-06-09 10:48:22 +0200

    added rule stats

--- statistics.h
+++ statistics.h
@@ -1,4 +1,39 @@
 typedef struct Stats Stats;
+typedef struct Rules Rules;
+
+#define RULES \
+\
+RULE (EP0) \
+RULE (EPN) \
+\
+RULE (BP0) \
+RULE (BN0F) \
+RULE (BN0L) \
+\
+RULE (AP0) \
+RULE (AP0F) \
+RULE (AP0L) \
+\
+RULE (JP0) \
+RULE (JN0F) \
+RULE (JN0L) \
+\
+RULE (DX) \
+RULE (DYS) \
+\
+RULE (UP) \
+RULE (UNXY) \
+RULE (UNT) \
+\
+RULE (FP) \
+RULE (FN)
+
+struct Rules {
+# define RULE(NAME) \
+  long NAME;
+  RULES
+#undef RULE
+};
 
 struct Stats {
   long decisions, flipped;
