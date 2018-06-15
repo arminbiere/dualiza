9898db8491e679bff78160b924c3231d8a73a02c
Armin Biere <biere@jku.at>
2018-06-15 07:08:11 +0200

    bug enforced

--- solver.c
+++ solver.c
@@ -315,7 +315,9 @@ static void enable_model_printing (Solver * solver, Name name) {
 
 static void check_options_fixed () {
   assert (!options.block || !options.dual || options.blocklimit <= 1);
+#if 0
   assert (!options.block || !options.discount || options.blocklimit <= 1);
+#endif
 }
 
 void fix_options () {
@@ -323,10 +325,12 @@ void fix_options () {
     msg (1, "dual propagation restricts blocking clause size to one");
     options.blocklimit = 1;
   } 
+#if 0
   if (options.block && options.discount && options.blocklimit > 1) {
     msg (1, "discounting restricts blocking clause size to one");
     options.blocklimit = 1;
   } 
+#endif
   check_options_fixed ();
 }
 
