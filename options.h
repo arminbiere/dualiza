54cd049730d20179269c1f2f29122eb655190787
Armin Biere <biere@jku.at>
2018-06-15 15:24:19 +0200

    another bug

--- options.h
+++ options.h
@@ -6,10 +6,10 @@
 OPTION (annotate,     0, "annotate generated") \
 OPTION (block,        1, "use blocking clauses") \
 OPTION (bump,         1, "bump variables (1=resolved, 2=reason)") \
-OPTION (blocklimit,   2, "blocking clause size limit") \
+OPTION (blocklimit,   1, "blocking clause size limit") \
 DBGOPT (check,        0, "enable expensive assertion checking") \
 OPTION (discount,     1, "discount models instead of backtracking") \
-OPTION (discountmax,  1, "maximum number of flipped levels discounted") \
+OPTION (discountmax,  0, "maximum number of discounted models") \
 OPTION (dual,         1, "dual SAT engine too (opposite of '--primal')") \
 OPTION (keepglue,     3, "keep all clause of this glue") \
 OPTION (keepsize,     3, "keep all clause of this size") \
