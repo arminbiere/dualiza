2a2509b3508447fddb5a18013db4af8f4846a254
Armin Biere <biere@jku.at>
2018-06-21 16:41:41 +0200

    nrp now bad for sharpSAT

--- formulas/nrp.c
+++ formulas/nrp.c
@@ -3,20 +3,22 @@
 int n;
 int main (int argc, char ** argv) {
   if (argc > 1) n = atoi (argv [1]);
-  if (n < 2) n = 2;
+  if (n < 3) n = 3;
   printf ("(x1");
   for (int i = 2; i <= n; i++)
     printf (" | x%d", i);
   printf (")");
-#if 1
-  for (int i = 1; i < n; i++) {
+  for (int i = 1; i <= n; i++) {
     printf (" |\n");
-    printf ("(x%d = x1", i + n);
-    for (int j = 2; j <= i + 1; j++)
-      printf (" ^ x%d", j);
+    printf ("(x%d", i + n);
+    int first = 1;
+    for (int j = 1; j <= n; j++) {
+      if (i == j) continue;
+      printf (" %c x%d", (first ? '=' : '^'), j);
+      first = 0;
+    }
     printf (")");
   }
-#endif
   printf ("\n");
   return 0;
 }
